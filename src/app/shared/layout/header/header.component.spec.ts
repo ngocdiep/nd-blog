14124f123465677ab64dd1b68945421aae67c177
nguyen ngoc diep <ngocdiep02tk@gmail.com>
2018-03-21 16:23:22 +0700

    fix and add the test case

--- src/app/shared/layout/header/header.component.spec.ts
+++ src/app/shared/layout/header/header.component.spec.ts
@@ -1,14 +1,31 @@
+import { RouterLinkDirectiveStub } from './../../../../../.vscode/testing/router-link-directive-stub';
+import { Observable } from 'rxjs/Observable';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { AuthService } from '../../services/auth.service';
+import { ScrollInformation } from '../../../app.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let authServiceStub: Partial<AuthService>;
+  let linkDes: DebugElement[];
+  let routerLinks: RouterLinkDirectiveStub[];
+
+  authServiceStub = {
+    isAuthenticated: Observable.of(false)
+  };
+
+  const expectedScrollInformation = Observable.of<ScrollInformation>({isUp: true, scrollTopHeight: 1});
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent, RouterLinkDirectiveStub],
+      providers: [{provide: AuthService, useValue: authServiceStub}],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -16,10 +33,20 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.scrollInformation = expectedScrollInformation;
     fixture.detectChanges();
+    linkDes = fixture.debugElement.queryAll(By.directive(RouterLinkDirectiveStub));
+    routerLinks = linkDes.map(de => de.injector.get(RouterLinkDirectiveStub));
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('can get RouterLink from template', () => {
+    expect(routerLinks.length).toBe(3, 'Should have 1 routerLink');
+    expect(routerLinks[0].linkParams).toBe('/');
+    expect(routerLinks[1].linkParams).toBe('/top');
+    expect(routerLinks[2].linkParams).toBe('/login');
   });
 });
